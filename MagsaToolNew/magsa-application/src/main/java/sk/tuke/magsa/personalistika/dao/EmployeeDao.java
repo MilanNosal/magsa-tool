de1efdf01c059fc2175596655a9fcfe9cf169162
sergej.chodarev <sergej.chodarev@3b58dd32-9299-ef35-c50d-1a8b06ab0579>
2015-02-03 14:09:58 +0000

    Remove generated project from the repository

--- MagsaToolNew/magsa-application/src/main/java/sk/tuke/magsa/personalistika/dao/EmployeeDao.java
+++
@@ -1,7 +0,0 @@
-package sk.tuke.magsa.personalistika.dao;
-
-import sk.tuke.magsa.framework.CRUDDao;
-import sk.tuke.magsa.personalistika.entity.Employee;
-
-public interface EmployeeDao extends CRUDDao<Employee> {
-}
--- MagsaToolNew/magsa-application/src/main/java/sk/tuke/magsa/personalistika/dao/ZamestnanecDao.java
+++
@@ -1,7 +0,0 @@
-package sk.tuke.magsa.personalistika.dao;
-
-import sk.tuke.magsa.framework.CRUDDao;
-import sk.tuke.magsa.personalistika.entity.Zamestnanec;
-
-public interface ZamestnanecDao extends CRUDDao<Zamestnanec> {
-}
