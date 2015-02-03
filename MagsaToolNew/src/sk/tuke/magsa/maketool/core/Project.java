663589ee8283c9ee9ecb68a5053a88a43fca7d22
sergej.chodarev <sergej.chodarev@3b58dd32-9299-ef35-c50d-1a8b06ab0579>
2015-02-03 14:08:13 +0000

    Refactor Maven invokation

--- MagsaToolNew/src/sk/tuke/magsa/maketool/core/Project.java
+++ MagsaToolNew/src/sk/tuke/magsa/maketool/core/Project.java
@@ -44,10 +44,7 @@ public class Project {
     @ProjectCompilation(Process.BUILDING)
     private void buildMavenProject() {
         try {
-            InvocationRequest request = new DefaultInvocationRequest();
-            File pom = new File(MagsaConfig.getInstance().getProjectPath() + "/magsa-generator/pom.xml");
-            request.setPomFile(pom);
-            request.setGoals(Collections.singletonList("compile"));
+            InvocationRequest request = createMavenRequest("compile");
             mavenInvoker.execute(request);
         } catch (MavenInvocationException ex) {
             System.err.println("A problem with Maven build:");
@@ -58,22 +55,15 @@ public class Project {
     public URL[] getClassPath() {
         final List<URL> urls = new ArrayList<>();
         try {
-            urls.add(new URL("file:///" + MagsaConfig.getInstance().getProjectPath() + "/magsa-generator/target/classes/"));
-            InvocationRequest request = new DefaultInvocationRequest();
-            File pom = new File(MagsaConfig.getInstance().getProjectPath() + "/magsa-generator/pom.xml");
-            request.setPomFile(pom);
-            request.setGoals(Collections.singletonList("dependency:build-classpath"));
+            urls.add(new File(MagsaConfig.getInstance().getProjectPath() +
+                    "/magsa-generator/target/classes/").toURI().toURL());
+            InvocationRequest request = createMavenRequest("dependency:build-classpath");
             request.setOutputHandler(new InvocationOutputHandler() {
                 @Override
                 public void consumeLine(String line) {
                     if (!line.startsWith("[")) {
-                        for (String path : line.split(":")) {
-                            try {
-                                urls.add(new URL("file://" + path));
-                            } catch (MalformedURLException ex) {
-                                Logger.getLogger(Project.class.getName()).log(Level.SEVERE, null, ex);
-                            }
-                        }
+                        List<URL> dependencies = parseClassPath(line);
+                        urls.addAll(dependencies);
                     }
                 }
             });
@@ -85,5 +75,25 @@ public class Project {
             Logger.getLogger(Project.class.getName()).log(Level.SEVERE, null, ex);
         }
         return urls.toArray(new URL[urls.size()]);
+    }
+
+    private InvocationRequest createMavenRequest(String goal) {
+        InvocationRequest request = new DefaultInvocationRequest();
+        File pom = new File(MagsaConfig.getInstance().getProjectPath() + "/magsa-generator/pom.xml");
+        request.setPomFile(pom);
+        request.setGoals(Collections.singletonList(goal));
+        return request;
+    }
+
+    private List<URL> parseClassPath(String pathLine) {
+        List<URL> urls = new ArrayList<>();
+        for (String path : pathLine.split(File.pathSeparator)) {
+            try {
+                urls.add(new File(path).toURI().toURL());
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(Project.class.getName()).log(Level.WARNING, null, ex);
+            }
+        }
+        return urls;
     }
 }
